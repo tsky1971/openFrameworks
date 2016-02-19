1ec98a00962b7ffae28a3713689fc2bbd7d888e9
tsky <thomas.kollakowsky@w-hs.de>
2016-02-19 16:58:29 +0100

    removechange to my templetes

--- scripts/templates/vs/src/ofApp.h
+++ scripts/templates/vs/src/ofApp.h
@@ -1,24 +1,45 @@
+/*
+*
+* Copyright (C) <2014> <w-hs - cglab projects>
+* All rights reserved.
+*
+* This software may be modified and distributed under the terms
+* of the BSD license.  See the LICENSE file for details.
+*/
+//////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////
+
 #pragma once
+//////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////
 
 #include "ofMain.h"
+//////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp
+{
+public:
+	void setup();
+	void exit();
+	void update();
+	void draw();
 
-	public:
-		void setup();
-		void update();
-		void draw();
-
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void mouseMoved(int x, int y );
+	void mouseDragged(int x, int y, int button);
+	void mousePressed(int x, int y, int button);
+	void mouseReleased(int x, int y, int button);
+	void mouseEntered(int x, int y);
+	void mouseExited(int x, int y);
+	void windowResized(int w, int h);
+	void dragEvent(ofDragInfo dragInfo);
+	void gotMessage(ofMessage msg);		
 };
+//////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////
